4b9df4582027fcd2b85a64a5c2e6f05d136f549f
JuanCanchila <Juan.canchila@gmail.com>
2023-07-18 13:53:59 -0500

    update handler

--- validar_fechas/src/Pluguin/WebformHandler/validar_fechas.php
+++ validar_fechas/src/Pluguin/WebformHandler/validar_fechas.php
@@ -9,7 +9,7 @@ use Drupal\webform\WebformSubmissionInterface;
  * Webform Date Validation handler.
  *
  * @WebformHandler(
- *   id = "validarfechas",
+ *   id = "handler_custom",
  *   label = @Translation("My awesome custom handler"),
  *   category = @Translation("Custom"),
  *   description = @Translation("Example of custom handler."),
@@ -20,14 +20,26 @@ use Drupal\webform\WebformSubmissionInterface;
  */
 class validar_fechas extends WebformHandlerBase {
 
-/**
-   * {@inheritdoc}
-   */
-  public function validateForm(array &$form, FormStateInterface $form_state, WebformSubmissionInterface $webform_submission) {
-    return true;
-  }
+    /**
+     * {@inheritdoc}
+     */
+    public function validateForm(array &$form, FormStateInterface $form_state, WebformSubmissionInterface $webform_submission) {
 
+        parent::validateForm($form, $form_state, $webform_submission);
 
+        if (!$form_state->hasAnyErrors()) {
+            //Tu validación aquí
+        }
+    }
 
+    /**
+     * {@inheritdoc}
+     */
+    public function submitForm(array &$form, FormStateInterface $form_state, WebformSubmissionInterface $webform_submission) {
+
+        if (!$form_state->hasAnyErrors()) {
+            //Tu logica despues del submit
+        }
+    }
 
 }
