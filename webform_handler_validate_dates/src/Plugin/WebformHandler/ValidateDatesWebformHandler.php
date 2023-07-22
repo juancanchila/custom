7c2c376d9c22bf91ca3f73a10e70392d4db4f418
JLopez <Juan.canchila@gmail.com>
2023-07-22 09:58:34 -0500

    dif

--- webform_handler_validate_dates/src/Plugin/WebformHandler/ValidateDatesWebformHandler.php
+++ webform_handler_validate_dates/src/Plugin/WebformHandler/ValidateDatesWebformHandler.php
@@ -46,7 +46,8 @@ class ValidateDatesWebformHandler extends WebformHandlerBase {
 			$date1 =new DrupalDateTime( $form_state->getValue('fecha_inicio'));
             $date2 = new DrupalDateTime($form_state->getValue('fecha_final'));
 
-			$diff = $date1->diff($date2);
+			$diff =  $date1->diff($date2);
+
 
 
 
