2db51031a514c723d2ed780a18369f9975fc00eb
JLopez <Juan.canchila@gmail.com>
2023-07-23 08:38:12 -0500

    fix validations 3 back

--- webform_handler_validate_dates/src/Plugin/WebformHandler/ValidateDatesWebformHandler.php
+++ webform_handler_validate_dates/src/Plugin/WebformHandler/ValidateDatesWebformHandler.php
@@ -34,7 +34,7 @@ class ValidateDatesWebformHandler extends WebformHandlerBase {
 
         parent::validateForm($form, $form_state, $webform_submission);
 
-        $alert_near ='<div class="alertaproximidad">Tenga en cuenta la fecha de su envento antes de liquidar. Su Solicitud tiene un tiempo de respuesta de 15 dias habiles Contados a partir de la fecha en la que sea adjuntado el soporte de pago y la documentción requerida en el formumlario, De conformidad con la ley 1437 del 2011</div>';
+  $alert='<div class="alertaproximidad">Tenga en cuenta la fecha de su envento antes de liquidar. Su Solicitud tiene un tiempo de respuesta de 15 dias habiles Contados a partir de la fecha en la que sea adjuntado el soporte de pago y la documentción requerida en el formumlario, De conformidad con la ley 1437 del 2011</div>';
 
 
         if (!$form_state->hasAnyErrors()) {
@@ -49,20 +49,61 @@ class ValidateDatesWebformHandler extends WebformHandlerBase {
             $diff_dias = $date1->diff( $date2);
             $diff_dias_hoy = $hoy->diff($date1);
             $this->messenger()->addError($this->t("Print: ".    $diff_dias->format('%R%a days')." /: ".$diff_dias_hoy->format('%R%a days (red)')));
+          /*
+			$date1 =new DrupalDateTime( $form_state->getValue('fecha_inicio'));
+            $date2 = new DrupalDateTime($form_state->getValue('fecha_final'));
+
+            $diff = strval($date1->formatTimeDiffSince($date2));
+
+            $this->messenger()->addStatus($this->t("Print:".$diff ));
+
+			$id_legal = $form_state->getValue('ndeg_de_documento_de_representante_legal_');
+
+            $hoy  = new DrupalDateTime('now');
+            $diff_dias_now = $date1->diff($hoy);
+			$diff_dias = $date1->diff($hoy->modify('+12 day'));
+
+            $this->messenger()->addStatus($this->t("Print:".$date1));
+*/
+
+           $comparison ="<=";
+
+        if ($date1 && $date2) {
 
 
+            switch ($comparison) {
+                case "==":
+                    $result = ($date1 == $date2) ? TRUE : FALSE;
+
+                    break;
+                case "<=":
+                    $result = ($date1 <= $date2) ? TRUE : FALSE;
+
+                    break;
+                case "<":
+                    $result = ($date1 < $date2) ? TRUE : FALSE;
+
+                    break;
+                case ">=":
+                    $result = ($date1 >= $date2) ? TRUE : FALSE;
+
+                    break;
+                case ">":
+                    $result = ($date1 > $date2) ? TRUE : FALSE;
+
+                    break;
+            }
 
 			//Imprimir Errores
 
-            if ($date2 <  $date1 ) {
-                $form_state->setErrorByName($this->form['fecha_final'], "Error en las fechas " );
+            if ($result == FALSE) {
+                $form_state->setErrorByName($this->form['fecha_final'], "Error en las fechas  ".$date1. "/ ".$date1 );
 
             }
-			  if ($diff_dias->format("%r%a")< 10) {
-
-                $this->messenger()->addError(           $this->t($alert_near)
-
-                );
+			  if ($diff_dias < 10) {
+				  //no alertar - > solo imprimir
+              // $form_state->setErrorByName($this->form['fecha_final'], "Su solicitud es menos a 10 dias / ". date('Y-m-d', $hoy));
+			//$this->messenger()->addStatus($this->t('Su solicitud es menos a 10 dias'));
             }
 
 			 if (!is_numeric($id_legal)) {
