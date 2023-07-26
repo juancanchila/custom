576c6f6c14ea14899e7897de243f932f6e80458a
JuanCanchila <Juan.canchila@gmail.com>
2023-07-26 12:08:49 -0500

    update

--- webform_handler_validate_dates/src/Plugin/WebformHandler/ValidateDatesWebformHandler.php
+++ webform_handler_validate_dates/src/Plugin/WebformHandler/ValidateDatesWebformHandler.php
@@ -37,11 +37,11 @@ class ValidateDatesWebformHandler extends WebformHandlerBase {
 
     public function validateForm(array &$form, FormStateInterface $form_state, WebformSubmissionInterface $webform_submission) {
 
-        
+
         parent::validateForm($form, $form_state, $webform_submission);
 
   $alert_near ='<div class="alertaproximidad">Tenga en cuenta la fecha de su envento antes de liquidar. Su Solicitud tiene un tiempo de respuesta de 15 dias habiles Contados a partir de la fecha en la que sea adjuntado el soporte de pago y la documentción requerida en el formumlario, De conformidad con la ley 1437 del 2011</div>';
- 
+
 
         if (!$form_state->hasAnyErrors()) {
             //Tu validación aquí
@@ -50,22 +50,29 @@ class ValidateDatesWebformHandler extends WebformHandlerBase {
             $date1 =new DrupalDateTime( $form_state->getValue('fecha_inicio'));
             $date2 = new DrupalDateTime($form_state->getValue('fecha_final'));
            $cantidad_dias = $form_state->getValue('duracion_del_evento_den_dias');
-         
-          
+
+
             $hoy = new DrupalDateTime('now');
             $diff_dias = $date1->diff( $date2);
+
             if($diff_dias->format("%r%a") == 0){
                 $diff_dias = $diff_dias->format("%r%a")+ 1;
             }else{
 
                 $diff_dias = $diff_dias->format("%r%a");
             }
+
+
+
+
             $diff_dias_hoy = $hoy->diff($date1);
             $duracion_del_evento_den_dias =$form_state->getValue('duracion_del_evento_den_dias');
             $valor_del_la_inversion = $form_state->getValue('valor_del_la_inversion');
-            $this->messenger()->addStatus($this->t("Print:".$form_state->getValue('tipo_de_solicitante') ));
-            $this->messenger()->addStatus($this->t("Print n días:". $cantidad_dias ));
-            $this->messenger()->addStatus($this->t("Print n días:". $diff_dias->format("%r%a") ));
+
+
+
+
+
           /*
            $this->messenger()->addStatus($this->t("Print:".$diff ));
 			$date1 =new DrupalDateTime( $form_state->getValue('fecha_inicio'));
@@ -88,15 +95,6 @@ class ValidateDatesWebformHandler extends WebformHandlerBase {
 
 
 
-           if( $page == 'datos_de_contacto' && $id_legal ){
-            //Imprimir Errores Contacto
-       if (!is_numeric($id_legal)) {
-            //no alertar - > solo imprimir
-        // $form_state->setErrorByName($this->form['ndeg_de_documento_de_representante_legal_'], "Validar doc Rep Legal");
-
-      }
-  }//errores de Contacto
-
 
 
             if( $page == 'datos_del_evento' ){
@@ -109,10 +107,11 @@ class ValidateDatesWebformHandler extends WebformHandlerBase {
             $this->messenger()->addError($this->t($alert_near));
       }
 
-     
-      if (  $diff_dias != $cantidad_dias ) {
-        $form_state->setErrorByName($this->$form['duracion_del_evento_den_dias'], "Error en la Cantidad de Días " );
+
+      if (  $diff_dias != $cantidad_dias  ) {
+        $form_state->setErrorByName($this->$form['duracion_del_evento_den_dias'], "Error en la Cantidad de Días, Se calculan : ".$diff_dias );
   }
+
 
             } //errores del evento
         }
@@ -126,8 +125,10 @@ class ValidateDatesWebformHandler extends WebformHandlerBase {
     public function submitForm(array &$form, FormStateInterface $form_state, WebformSubmissionInterface $webform_submission) {
 
         $this->submitMyFieldData($webform_submission);
+        $this->valor_a_pagar($form_state);
+
         if (!$form_state->hasAnyErrors()) {
-           
+
             //Tu logica despues del submit
 
 
@@ -158,4 +159,137 @@ class ValidateDatesWebformHandler extends WebformHandlerBase {
 
   }
 
+ public function money_format_fild($money) {
+
+    $cleanString = preg_replace('/([^0-9\.,])/i', '', $money);
+    $onlyNumbersString = preg_replace('/([^0-9])/i', '', $money);
+
+    $separatorsCountToBeErased = strlen($cleanString) - strlen($onlyNumbersString) - 1;
+
+    $stringWithCommaOrDot = preg_replace('/([,\.])/', '', $cleanString, $separatorsCountToBeErased);
+    $removedThousandSeparator = preg_replace('/(\.|,)(?=[0-9]{3,}$)/', '',  $stringWithCommaOrDot);
+
+    $money_clean = (float) str_replace(',', '.', $removedThousandSeparator);
+
+    return $money_clean;
+   // $this->messenger()->addStatus($this->t("Print:". $money_clean));
+  }
+
+
+
+ public function valor_a_pagar( $form_state) {
+
+
+
+ //calculos
+ $vocabulary_name = 'smlv';
+ $query = \Drupal::entityQuery('taxonomy_term');
+ $query->condition('vid', $vocabulary_name);
+ $tids = $query->execute();
+ $terms = Term::loadMultiple($tids);
+
+
+ foreach ($terms as $term) {
+   $id2 = $term->getFields();
+       $value  = $term->get('field_smlv')->getValue();
+       $valor =$value[0]["value"];
+ }
+ $valor =$value[0]["value"];
+
+
+ $valor_liquidacion = $this->money_format_fild( $form_state->getValue('valor_del_la_inversion'));
+ $numero_dias = $form_state->getValue('duracion_del_evento_den_dias');
+
+
+ $this->messenger()->addStatus($this->t("SMLV: $". number_format( $valor , 2, ',', '.')));
+ $this->messenger()->addStatus($this->t("Valor Evento: $" . number_format( $valor_liquidacion , 2, ',', '.')));
+ $this->messenger()->addStatus($this->t("Días:".   $numero_dias));
+
+ $valor_tarifa_evento_25 = $valor * 25 ;
+ $valor_tarifa_evento_35 = $valor * 35 ;
+ $valor_tarifa_evento_50 = $valor * 50 ;
+ $valor_tarifa_evento_70 = $valor * 70 ;
+ $valor_tarifa_evento_100 = $valor * 100 ;
+ $valor_tarifa_evento_200 = $valor * 200 ;
+ $valor_tarifa_evento_300 = $valor * 300 ;
+ $valor_tarifa_evento_400 = $valor * 400 ;
+ $valor_tarifa_evento_500 = $valor * 500 ;
+ $valor_tarifa_evento_700 = $valor * 700 ;
+ $valor_tarifa_evento_900 = $valor * 900 ;
+ $valor_tarifa_evento_1500 = $valor * 1500 ;
+ $valor_tarifa_evento_2115 = $valor * 2115 ;
+ $valor_tarifa_evento_8458 = $valor * 8458 ;
+
+
+
+
+if ($valor_liquidacion < $valor_tarifa_evento_25) {
+$valor_tarifa = 118561;//ok
+$valor_liquidacion = 118561 *  $numero_dias ;
+$valor_liquidacion_r = 118600 *  $numero_dias ;
+} elseif ($valor_liquidacion  >= $valor_tarifa_evento_25  && $valor_liquidacion < $valor_tarifa_evento_35) {
+ $valor_tarifa = 166176;//ok
+$valor_liquidacion = 166176  *  $numero_dias ;
+$valor_liquidacion_r = 166200 *  $numero_dias ;
+}elseif ($valor_liquidacion  >= $valor_tarifa_evento_35  && $valor_liquidacion < $valor_tarifa_evento_50) {
+ $valor_tarifa =237598;
+$valor_liquidacion =237598  *  $numero_dias;
+$valor_liquidacion_r = 237600 *  $numero_dias ;
+
+}elseif ($valor_liquidacion  >= $valor_tarifa_evento_50  && $valor_liquidacion < $valor_tarifa_evento_70 ) {
+ $valor_tarifa =332829;
+$valor_liquidacion = 332829  *  $numero_dias ;
+$valor_liquidacion_r =  332850 *  $numero_dias ;
+}elseif ($valor_liquidacion  >= $valor_tarifa_evento_70  && $valor_liquidacion < $valor_tarifa_evento_100) {
+ $valor_tarifa =  475672;
+$valor_liquidacion = 475672  *  $numero_dias ;
+$valor_liquidacion_r =  475700  *  $numero_dias ;
+}elseif ($valor_liquidacion  >= $valor_tarifa_evento_100  && $valor_liquidacion < $valor_tarifa_evento_200) {
+ $valor_tarifa =  951804;
+$valor_liquidacion = 951804 * $numero_dias;
+$valor_liquidacion_r =  951800  *  $numero_dias ;
+}elseif ($valor_liquidacion  >= $valor_tarifa_evento_200  && $valor_liquidacion < $valor_tarifa_evento_300) {
+ $valor_tarifa = 1427971;
+$valor_liquidacion = 1427971 *  $numero_dias;
+$valor_liquidacion_r =  1428000  *  $numero_dias ;
+}elseif ($valor_liquidacion  >= $valor_tarifa_evento_300  && $valor_liquidacion < $valor_tarifa_evento_400) {
+ $valor_tarifa = 1904121;
+$valor_liquidacion =  1904121  *  $numero_dias ;
+$valor_liquidacion_r =  1904150  *  $numero_dias ;
+}elseif ($valor_liquidacion  >= $valor_tarifa_evento_400  && $valor_liquidacion < $valor_tarifa_evento_500) {
+ $valor_tarifa = 2380269;
+$valor_liquidacion =  2380269  *  $numero_dias  ;
+$valor_liquidacion_r = 2380300  *  $numero_dias;
+}elseif ($valor_liquidacion  >= $valor_tarifa_evento_500  && $valor_liquidacion < $valor_tarifa_evento_700) {
+ $valor_tarifa =3332567 ;
+$valor_liquidacion = 3332567 *  $numero_dias ;
+$valor_liquidacion_r = 3332600 *  $numero_dias;
+}elseif ($valor_liquidacion  >= $valor_tarifa_evento_700  && $valor_liquidacion < $valor_tarifa_evento_900) {
+ $valor_tarifa = 4284866;
+$valor_liquidacion = 4284866  *  $numero_dias ;
+$valor_liquidacion_r = 4284900 *  $numero_dias;
+}elseif ($valor_liquidacion  >= $valor_tarifa_evento_900  && $valor_liquidacion < $valor_tarifa_evento_1500) {
+ $valor_tarifa =8160204;
+$valor_liquidacion = 8160204  *  $numero_dias ;
+$valor_liquidacion_r = 8160200 *  $numero_dias;
+}elseif ($valor_liquidacion >= $valor_tarifa_evento_1500  && $valor_liquidacion < $valor_tarifa_evento_2115) {
+$valor_tarifa =10917526;
+$valor_liquidacion = 10917526 *  $numero_dias;
+$valor_liquidacion_r = 10917550 *  $numero_dias;
+}elseif ($valor_liquidacion  >= $valor_tarifa_evento_2115  && $valor_liquidacion < $valor_tarifa_evento_8458) {
+$valor_tarifa =37374939;
+$valor_liquidacion =37374939 *  $numero_dias;
+$valor_liquidacion_r =37374939 *  $numero_dias;
+
+}else {
+/*$valor_tarifa =($valor_evento * 0.4)/100;
+$valor_liquidacion = ( ($valor_evento * 0.4)/100) ;*/
+$valor_tarifa =208879615;
+/* $valor_liquidacion =37374939 *  $numero_dias;
+$valor_liquidacion_r =37374939 *  $numero_dias;*/
+}
+$valor = $valor_liquidacion;
+   $this->messenger()->addStatus($this->t("Valor Liquidacion: $". number_format($valor_liquidacion , 2, ',', '.')));
+   $this->messenger()->addStatus($this->t("Valor Tarifa: $". number_format($valor_tarifa , 2, ',', '.')));
+ }
 }
