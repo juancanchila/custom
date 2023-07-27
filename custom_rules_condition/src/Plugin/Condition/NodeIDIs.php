dcd11f0bb3784fa7528010faaee4a63ba9a816b4
JuanCanchila <Juan.canchila@gmail.com>
2023-07-27 09:39:54 -0500

    new module +1

---
+++ custom_rules_condition/src/Plugin/Condition/NodeIDIs.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace Drupal\custom_rules_condition\Plugin\Condition;
+
+use Drupal\node\NodeInterface;
+use Drupal\rules\Core\RulesConditionBase;
+
+/**
+ * Provides a 'Node ID is' condition.
+ *
+ * @Condition(
+ *   id = "custom_rules_condition",
+ *   label = @Translation("Node ID is"),
+ *   category = @Translation("Node"),
+ *   context = {
+ *     "node" = @ContextDefinition("entity:node",
+ *       label = @Translation("Node")
+ *     ),
+ *     "ids" = @ContextDefinition("string",
+ *       label = @Translation("IDs of Nodes"),
+ *       description = @Translation("IDs of Nodes"),
+ *       multiple = TRUE
+ *     )
+ *   }
+ * )
+ *
+ */
+class NodeIDIs extends RulesConditionBase
+{
+
+    /**
+     * @param \Drupal\node\NodeInterface $node
+     *   The node to check.
+     *
+     * @return bool
+     *   TRUE if the node id existe in $ids array.
+     */
+    protected function doEvaluate(NodeInterface $node, $ids)
+    {
+        return in_array($node->id(),$ids);
+    }
+
+
+}
