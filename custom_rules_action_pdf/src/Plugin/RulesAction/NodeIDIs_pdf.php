3a1cd92a879b40fa6c0e74b8bdfd8ea59b340875
juancanchila <juan.canchila@gmail.com>
2023-08-23 14:44:17 -0500

    action base

--- custom_rules_action_pdf/src/Plugin/RulesAction/NodeIDIs_pdf.php
+++ custom_rules_action_pdf/src/Plugin/RulesAction/NodeIDIs_pdf.php
@@ -4,32 +4,7 @@ namespace Drupal\custom_rules_action_pdf\Plugin\RulesAction;
 
 use Drupal\node\NodeInterface;
 use Drupal\rules\Core\RulesActionBase;
-use Drupal\Core\Entity\EntityInterface;
-use Drupal\Core\Datetime\DrupalDateTime;
 
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\Mailer\MailerInterface;
-use Symfony\Component\Mime\Email;
-use Symfony\Component\Routing\Annotation\Route;
-use Drupal\Component\Utility\EmailValidator;
-use Drupal\Core\Url;
-
-use Drupal\Core\Form\FormStateInterface;
-use Drupal\Core\Form\FormBase;
-use Drupal\Core\Mail\MailManagerInterface;
-
-
-use Symfony\Component\DependencyInjection\ContainerInterface;
-use Drupal\Core\Language\LanguageManagerInterface;
-use Drupal\taxonomy\Entity\Term;
-use Drupal\node\Entity\Node;
-use Drupal\file\Entity\File;
-
-use Drupal\Core\Cache\CacheTagsInvalidatorInterface;
-
-use Drupal\Core\Link;
-use Symfony\Component\HttpFoundation\Session\SessionInterface;
 
 
 
