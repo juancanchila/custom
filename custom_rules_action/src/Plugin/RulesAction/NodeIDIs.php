6c6aebf9da742f86f7a97dbc0f20a1024f3a5a84
juancanchila <juan.canchila@gmail.com>
2023-08-23 09:36:11 -0500

    txt

--- custom_rules_action/src/Plugin/RulesAction/NodeIDIs.php
+++ custom_rules_action/src/Plugin/RulesAction/NodeIDIs.php
@@ -31,8 +31,7 @@ use Drupal\Core\Cache\CacheTagsInvalidatorInterface;
 use Drupal\Core\Link;
 use Symfony\Component\HttpFoundation\Session\SessionInterface;
 
-use Drupal\Core\StreamWrapper\PrivateStream;
-
+use Drupal\Core\File\FileSystemInterface;
 
 /**
  * Provides a 'Node ID is' condition.
@@ -53,7 +52,11 @@ use Drupal\Core\StreamWrapper\PrivateStream;
  */
 class NodeIDIs extends RulesActionBase
 {
- /**
+
+
+
+
+   /**
    * Executes the action with the given context.
    *
    * @param \Drupal\node\NodeInterface $node
@@ -62,6 +65,7 @@ class NodeIDIs extends RulesActionBase
    */
   protected function doExecute(NodeInterface $node) {
 
+  
     $hoy =new DrupalDateTime( 'now');
 
     /** Obteniendo el field_consecutivo_factura del nodo creado */
@@ -90,15 +94,11 @@ class NodeIDIs extends RulesActionBase
 
       $path = "sites/default/files/tmp/mpdf";
      
-
-      $output = 'Test Text';
-      $file_save_path_stream_directory =  'private://gmt';
-      
-      file_prepare_directory($file_save_path_stream_directory, FILE_CREATE_DIRECTORY | FILE_MODIFY_PERMISSIONS);
-      
-      $fileLocation = $file_save_path_stream_directory . '/test.txt';
-      
-      $file = file_save_data($output, $fileLocation, FILE_EXISTS_REPLACE); 
+      $data = 'Text file example content';
+      /** @var \Drupal\file\FileRepositoryInterface $fileRepository */
+      $fileRepository = \Drupal::service('file.repository');
+      $fileRepository->writeData($data, "public://my-dir/MY_FILE.txt", FileSystemInterface::EXISTS_REPLACE);
+ 
 
 /*
 
