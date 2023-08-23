7592f2d49146299ed63b5f8cca04f19fe9bb2861
juancanchila <juan.canchila@gmail.com>
2023-08-23 13:56:18 -0500

    nada

--- custom_rules_action_pdf/src/Plugin/RulesAction/NodeIDIs_pdf.php
+++ custom_rules_action_pdf/src/Plugin/RulesAction/NodeIDIs_pdf.php
@@ -2,7 +2,34 @@
 
 namespace Drupal\custom_rules_action_pdf\Plugin\RulesAction;
 
+use Drupal\node\NodeInterface;
+use Drupal\rules\Core\RulesActionBase;
+use Drupal\Core\Entity\EntityInterface;
+use Drupal\Core\Datetime\DrupalDateTime;
 
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Mailer\MailerInterface;
+use Symfony\Component\Mime\Email;
+use Symfony\Component\Routing\Annotation\Route;
+use Drupal\Component\Utility\EmailValidator;
+use Drupal\Core\Url;
+
+use Drupal\Core\Form\FormStateInterface;
+use Drupal\Core\Form\FormBase;
+use Drupal\Core\Mail\MailManagerInterface;
+
+
+use Symfony\Component\DependencyInjection\ContainerInterface;
+use Drupal\Core\Language\LanguageManagerInterface;
+use Drupal\taxonomy\Entity\Term;
+use Drupal\node\Entity\Node;
+use Drupal\file\Entity\File;
+
+use Drupal\Core\Cache\CacheTagsInvalidatorInterface;
+
+use Drupal\Core\Link;
+use Symfony\Component\HttpFoundation\Session\SessionInterface;
 
 
 
@@ -15,23 +42,36 @@ namespace Drupal\custom_rules_action_pdf\Plugin\RulesAction;
  *   label = @Translation("get pdf"),
  *   category = @Translation("Node"),
  * context_definitions = {
-
+ *     "node" = @ContextDefinition("entity:node",
+ *       label = @Translation("Node"),
+ *       description = @Translation("Specifies the content item to change."),
+ *       assignment_restriction = "selector"
+ *     ),
  *   }
  * )
  *
  */
-
-
 class NodeIDIs_pdf extends RulesActionBase
 {
+ /**
+   * Executes the action with the given context.
+   *
+   * @param \Drupal\node\NodeInterface $node
+   *   The node to modify.
+   *
+   */
+  protected function doExecute(NodeInterface $node) {
 
-  protected function doExecute() {
 
-    $message_info = "Se ha actualizado la Liquidación # ";
-    \Drupal::messenger()->addMessage(t( $message_info), 'warning');
     }
 
-
+ /**
+   * {@inheritdoc}
+   */
+  public function autoSaveContext() {
+    // The node should be auto-saved after the execution.
+    return ['node'];
+  }
 
 
 
