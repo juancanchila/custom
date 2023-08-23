2cc9aee6df9ff6012cf5887deaa598dc615afe70
juancanchila <juan.canchila@gmail.com>
2023-08-23 13:54:06 -0500

    edit

--- custom_rules_action_pdf/src/Plugin/RulesAction/NodeIDIs_pdf.php
+++ custom_rules_action_pdf/src/Plugin/RulesAction/NodeIDIs_pdf.php
@@ -2,36 +2,7 @@
 
 namespace Drupal\custom_rules_action_pdf\Plugin\RulesAction;
 
-use Drupal\user\UserInterface;
-use Drupal\rules\Core\RulesActionBase;
-use Drupal\Core\Entity\EntityInterface;
-use Drupal\Core\Datetime\DrupalDateTime;
 
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\Mailer\MailerInterface;
-use Symfony\Component\Mime\Email;
-use Symfony\Component\Routing\Annotation\Route;
-use Drupal\Component\Utility\EmailValidator;
-use Drupal\Core\Url;
-
-use Drupal\Core\Form\FormStateInterface;
-use Drupal\Core\Form\FormBase;
-use Drupal\Core\Mail\MailManagerInterface;
-
-
-use Symfony\Component\DependencyInjection\ContainerInterface;
-use Drupal\Core\Language\LanguageManagerInterface;
-use Drupal\taxonomy\Entity\Term;
-use Drupal\node\Entity\Node;
-use Drupal\file\Entity\File;
-
-use Drupal\Core\Cache\CacheTagsInvalidatorInterface;
-
-use Drupal\Core\Link;
-use Symfony\Component\HttpFoundation\Session\SessionInterface;
-
-use Drupal\Core\File\FileSystemInterface;
 
 
 
@@ -44,7 +15,7 @@ use Drupal\Core\File\FileSystemInterface;
  *   label = @Translation("get pdf"),
  *   category = @Translation("Node"),
  * context_definitions = {
- *  "user" = @ContextDefinition("entity:user", label = @Translation("User")),
+
  *   }
  * )
  *
@@ -53,26 +24,14 @@ use Drupal\Core\File\FileSystemInterface;
 
 class NodeIDIs_pdf extends RulesActionBase
 {
- /**
-   * Executes the action with the given context.
-   *
-   * @param \Drupal\node\UserInterface $node
-   *   The node to modify.
-   *
-   */
-  protected function doExecute(UserInterface $user) {
+
+  protected function doExecute() {
 
     $message_info = "Se ha actualizado la Liquidación # ";
     \Drupal::messenger()->addMessage(t( $message_info), 'warning');
     }
 
- /**
-   * {@inheritdoc}
-   */
-  public function autoSaveContext() {
-    // The node should be auto-saved after the execution.
-    return ['node'];
-  }
+
 
 
 
