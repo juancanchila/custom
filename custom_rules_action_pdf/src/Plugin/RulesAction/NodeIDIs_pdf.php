67f077eed0a87cd925bd199d528d8c5dacff2487
juancanchila <juan.canchila@gmail.com>
2023-08-23 14:19:31 -0500

    alert

--- custom_rules_action_pdf/src/Plugin/RulesAction/NodeIDIs_pdf.php
+++ custom_rules_action_pdf/src/Plugin/RulesAction/NodeIDIs_pdf.php
@@ -42,14 +42,10 @@ use Symfony\Component\HttpFoundation\Session\SessionInterface;
  *   label = @Translation("get pdf"),
  *   category = @Translation("Node"),
  * context_definitions = {
- *   context = {
- *     "message" = @ContextDefinition("string",
- *       label = @Translation("Message"),
- *       description = @Translation("write your message"),
- *     ),
- *     "type" = @ContextDefinition("string",
- *       label = @Translation("Message type"),
- *       description = @Translation("Message type: status, warning or error "),
+ *     "node" = @ContextDefinition("entity:node",
+ *       label = @Translation("Node"),
+ *       description = @Translation("Specifies the content item to change."),
+ *       assignment_restriction = "selector"
  *     ),
  *   }
  * )
@@ -57,13 +53,26 @@ use Symfony\Component\HttpFoundation\Session\SessionInterface;
  */
 class NodeIDIs_pdf extends RulesActionBase
 {
-    /**
-     * @param $name
-     */
-    protected function doExecute($message, $type)
-    {
-        \Drupal::messenger()->addMessage(t($message), $type);
+ /**
+   * Executes the action with the given context.
+   *
+   * @param \Drupal\node\NodeInterface $node
+   *   The node to modify.
+   *
+   */
+  protected function doExecute(NodeInterface $node) {
+
+    $type = "Se ha creado la Liquidación # ".$sec;
+    \Drupal::messenger()->addMessage(t( $type), 'status');
     }
+
+ /**
+   * {@inheritdoc}
+   */
+  public function autoSaveContext() {
+    // The node should be auto-saved after the execution.
+    return ['node'];
+  }
 
 
 
