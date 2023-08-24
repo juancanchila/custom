b7a3a72e6ddba3c1cfa1c87920ca2797c6bc8619
JLopez <Juan.canchila@gmail.com>
2023-08-23 21:15:09 -0500

    NO DETALLE

--- custom_rules_action/src/Plugin/RulesAction/NodeIDIs.php
+++ custom_rules_action/src/Plugin/RulesAction/NodeIDIs.php
@@ -97,7 +97,7 @@ class NodeIDIs extends RulesActionBase
       $code="4157709998461239"."8020".$sec."3900".$this->money_format_fild($valor[0]["value"])."96".date('Y')."1231";
       $code_content="(415)7709998461239"."(8020)".$sec."(3900)".$this->money_format_fild($valor[0]["value"])."(96)".date('Y')."1231";
 
-      $field_detalle = $node->get('field_detalle')->getValue(); //direcciones y placas y especies
+      //$field_detalle = $node->get('field_detalle')->getValue(); //direcciones y placas y especies
       /*
       $cantidad = $node->get('field_cantidad')->getValue(); //AF
 
