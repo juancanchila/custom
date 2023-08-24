9cbd5b7a26ec1a967c1ba8dde200423673ca2124
JLopez <Juan.canchila@gmail.com>
2023-08-23 20:35:14 -0500

    cantidad

--- custom_rules_action/src/Plugin/RulesAction/NodeIDIs.php
+++ custom_rules_action/src/Plugin/RulesAction/NodeIDIs.php
@@ -98,7 +98,7 @@ class NodeIDIs extends RulesActionBase
       $code_content="(415)7709998461239"."(8020)".$sec."(3900)".$this->money_format_fild($valor[0]["value"])."(96)".date('Y')."1231";
 
       $field_detalle = $node->get('field_detalle')->getValue(); //direcciones y placas y especies
-      $cantidad = $node->get('$field_cantidad')->getValue(); //AF
+      $cantidad = $node->get('field_cantidad')->getValue(); //AF
 
 
       $field_nombre_predio  = $node->get('field_nombre_predio')->getValue(); // AF, RS,
