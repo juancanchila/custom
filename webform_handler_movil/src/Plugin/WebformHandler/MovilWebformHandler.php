c301225f219cfedf16b64f963d8ce7323bd8be85
JuanCanchila <Juan.canchila@gmail.com>
2023-08-25 14:15:09 -0500

    no array

--- webform_handler_movil/src/Plugin/WebformHandler/MovilWebformHandler.php
+++ webform_handler_movil/src/Plugin/WebformHandler/MovilWebformHandler.php
@@ -52,7 +52,7 @@ public function validateForm(array &$form, FormStateInterface $form_state, Webfo
 
 
 
-            $this->messenger()->addStatus($this->t("Test".$cantidad_placas[0]."/".$listado_placas[0]));
+            $this->messenger()->addStatus($this->t("Test".$cantidad_placas[0]."/".$listado_placas));
             //prueba 3
                }
       
