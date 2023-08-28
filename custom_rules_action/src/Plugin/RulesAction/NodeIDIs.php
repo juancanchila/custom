79a1c2d6bf919a348630ee6ddf2f55afa1155565
JuanCanchila <Juan.canchila@gmail.com>
2023-08-28 09:10:34 -0500

    no array

--- custom_rules_action/src/Plugin/RulesAction/NodeIDIs.php
+++ custom_rules_action/src/Plugin/RulesAction/NodeIDIs.php
@@ -97,6 +97,7 @@ class NodeIDIs extends RulesActionBase
       $code="4157709998461239"."8020".$sec."3900".$this->money_format_fild($valor[0]["value"])."96".date('Y')."1231";
       $code_content="(415)7709998461239"."(8020)".$sec."(3900)".$this->money_format_fild($valor[0]["value"])."(96)".date('Y')."1231";
       $concepto_ambiental_liquidacion =  $node->get('field_concepto_ambiental_liq')->getValue();
+      $concepto_ambiental_liquidacion =$concepto_ambiental_liquidacion[0];
       //$field_detalle = $node->get('field_detalle')->getValue(); //direcciones y placas y especies
       /*
       $cantidad = $node->get('field_cantidad')->getValue(); //AF
@@ -110,7 +111,7 @@ class NodeIDIs extends RulesActionBase
 */
      
 
- \Drupal::messenger()->addMessage(t( $concepto_ambiental_liquidacion[0]), 'status');
+ \Drupal::messenger()->addMessage(t($concepto_ambiental_liquidacion), 'status');
 
 
       switch ($concepto_ambiental_liquidacion[0]) {
