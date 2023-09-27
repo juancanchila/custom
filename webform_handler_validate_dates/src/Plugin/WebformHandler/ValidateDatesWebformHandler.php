00283c88c0f90bf4a4227176133a642fe067a4a8
JuanCanchila <Juan.canchila@gmail.com>
2023-09-27 10:09:45 -0500

    documentos

--- webform_handler_validate_dates/src/Plugin/WebformHandler/ValidateDatesWebformHandler.php
+++ webform_handler_validate_dates/src/Plugin/WebformHandler/ValidateDatesWebformHandler.php
@@ -52,7 +52,7 @@ public function submitForm(array &$form, FormStateInterface $form_state, Webform
 
 
 
-    if(  $page == 'datos_del_evento' ){
+    if(  $page == 'documentos' ){
       //   $this->submitMyFieldData($webform_submission);
             $this->validate_dates($form_state,$webform_submission);
          }
