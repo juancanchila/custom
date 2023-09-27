c70a638b609551109e924c1dad28702cf8984cc1
JuanCanchila <Juan.canchila@gmail.com>
2023-09-27 11:15:43 -0500

    validateFor

--- webform_handler_validate_dates/src/Plugin/WebformHandler/ValidateDatesWebformHandler.php
+++ webform_handler_validate_dates/src/Plugin/WebformHandler/ValidateDatesWebformHandler.php
@@ -39,7 +39,7 @@ public function validateForm(array &$form, FormStateInterface $form_state, Webfo
 
 
 
-    
+    $this->validate_dates($form_state,$webform_submission);
 
 }
 
@@ -51,12 +51,7 @@ public function submitForm(array &$form, FormStateInterface $form_state, Webform
 
 
 
-    if(  $page == 'datos_del_evento' ){
-      //   $this->submitMyFieldData($webform_submission);
-            $this->validate_dates($form_state,$webform_submission);
-          //$this->messenger()->addStatus($this->t("datos_del_evento"));
-         }
-
+   
 
 
     if(  $page == 'confirmacion' ){
