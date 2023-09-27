67452267c0cd86fe40dadd9278dd3e36a398c261
JuanCanchila <Juan.canchila@gmail.com>
2023-09-27 09:48:02 -0500

    $f_limit

--- webform_handler_validate_dates/src/Plugin/WebformHandler/ValidateDatesWebformHandler.php
+++ webform_handler_validate_dates/src/Plugin/WebformHandler/ValidateDatesWebformHandler.php
@@ -89,13 +89,16 @@ public function money_format_fild($money) {
     $now = DrupalDateTime::createFromTimestamp(time());
     $now->setTimezone(new \DateTimeZone('UTC'));
     
-    $f1 = DrupalDateTime::createFromTimestamp($form_state->getValue('fecha_inicial'));
+    $f1 = strtotime($form_state->getValue('fecha_inicial'));
     $cantidad_dias = $form_state->getValue('duracion_del_evento_den_dias');
-    $f_limit =  DrupalDateTime::createFromTimestamp($form_state->getValue('fecha_final'));
-    $dt =  DrupalDateTime::createFromTimestamp($now->format('Y-m-d'));
+    $f_limit = strtotime($form_state->getValue('fecha_final'));
+    $dt = strtotime($now->format('Y-m-d'));
     $diff = ($f_limit - $f1) / 86400;
     $diff02 = ($f1 - $dt) / 86400;
 
+    $f1 = DrupalDateTime::createFromTimestamp($f1);
+    $f_limit = DrupalDateTime::createFromTimestamp($f_limit);
+    
     if ($f1 > $f_limit) {
         // Use addError to display an alert message.
         $form_state->setErrorByName('fecha_inicial', $this->t('La fecha inicial no puede ser menor a la final'));
