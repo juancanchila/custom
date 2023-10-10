81da1f271371ad0e8ea6bfd52658ebbfb580c2fa
JLopez <Juan.canchila@gmail.com>
2023-10-10 14:02:37 -0500

    new rule

---
+++ custom_rules_action_transportador/src/Plugin/RulesAction/TransportadorID.php
@@ -0,0 +1,114 @@
+<?php
+
+namespace Drupal\custom_rules_action_transportador\Plugin\RulesAction;
+
+use Drupal\node\NodeInterface;
+use Drupal\rules\Core\RulesActionBase;
+use Drupal\Core\Entity\EntityInterface;
+use Drupal\Core\Datetime\DrupalDateTime;
+use Drupal\Core\Mail\MailManagerInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Mailer\MailerInterface;
+use Symfony\Component\Mime\Email;
+use Symfony\Component\Routing\Annotation\Route;
+use Drupal\Component\Utility\EmailValidator;
+use Drupal\Core\Url;
+
+use Drupal\Core\Form\FormStateInterface;
+use Drupal\Core\Form\FormBase;
+
+use Symfony\Component\DependencyInjection\ContainerInterface;
+use Drupal\Core\Language\LanguageManagerInterface;
+use Drupal\taxonomy\Entity\Term;
+use Drupal\node\Entity\Node;
+use Drupal\file\Entity\File;
+
+use Drupal\Core\Cache\CacheTagsInvalidatorInterface;
+
+use Drupal\Core\Link;
+use Symfony\Component\HttpFoundation\Session\SessionInterface;
+
+use Drupal\Core\File\FileSystemInterface;
+
+
+/**
+ * Provides a 'transportador action' condition.
+ *
+ * @RulesAction(
+ *   id = "custom_rules_action_establecimiento",
+ *   label = @Translation("Transportador ID"),
+ *   category = @Translation("Node"),
+ * context_definitions = {
+ *     "node" = @ContextDefinition("entity:node",
+ *       label = @Translation("Node"),
+ *       description = @Translation("Specifies the content item to change."),
+ *       assignment_restriction = "selector"
+ *     ),
+ *   }
+ * )
+ *
+ */
+class TransportadorID extends RulesActionBase
+{
+
+
+ /**
+   * Executes the action with the given context.
+   *
+   * @param \Drupal\node\NodeInterface $node
+   *   The node to modify.
+   *
+   */
+  //number_format( $valor_evento, 2, ',', '.');
+  protected function doExecute(NodeInterface $node) {
+
+// consultar si hay visitas para la vigencia actual
+// si no hay visitas crearlas
+// notificar a administrativa y al ususario
+
+
+
+/*
+
+$visita = Node::create(['type' => 'visita_cs']);
+$visita->set('title', 'Visita de Control y Seguimiento');
+$visita->set('field_establecimiento', $node->id());
+$visita->enforceIsNew();
+$visita->save();
+
+// Guarda el nodo en la base de datos.
+    $node->save();
+
+*/
+
+
+
+
+    }
+
+ /**
+   * {@inheritdoc}
+   */
+  public function autoSaveContext() {
+    // The node should be auto-saved after the execution.
+    return FALSE;
+  }
+
+  public function money_format_fild($money) {
+
+    $cleanString = preg_replace('/([^0-9\.,])/i', '', $money);
+    $onlyNumbersString = preg_replace('/([^0-9])/i', '', $money);
+
+    $separatorsCountToBeErased = strlen($cleanString) - strlen($onlyNumbersString) - 1;
+
+    $stringWithCommaOrDot = preg_replace('/([,\.])/', '', $cleanString, $separatorsCountToBeErased);
+    $removedThousandSeparator = preg_replace('/(\.|,)(?=[0-9]{3,}$)/', '',  $stringWithCommaOrDot);
+
+    $money_clean = (float) str_replace(',', '.', $removedThousandSeparator);
+
+    return $money_clean;
+   // $this->messenger()->addStatus($this->t("Print:". $money_clean));
+  }
+
+}
