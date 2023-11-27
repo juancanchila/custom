72818d279474353a1e739af98fa011970da6413d
JLopez <Juan.canchila@gmail.com>
2023-11-26 20:46:27 -0500

    dates

--- webform_handler_fija/src/Plugin/WebformHandler/FijaWebformHandler.php
+++ webform_handler_fija/src/Plugin/WebformHandler/FijaWebformHandler.php
@@ -225,15 +225,14 @@ if ($f1 > $f_limit) {
     $valor_liquidacion_r = 10917550 *   $numero_dias * $cantidad ;
   }elseif ($valor_liquidacion  > $valor_tarifa_evento_2115  && $valor_liquidacion <= $valor_tarifa_evento_8458) {
     $valor_tarifa =($valor_evento * 0.5)/100;
-
-  }elseif ($valor_liquidacion  = $valor_tarifa_evento_2115 ) {
+    $valor_liquidacion =($valor_evento * 0.5)/100;
+  }elseif ($valor_liquidacion  == $valor_tarifa_evento_2115 ) {
     $valor_tarifa =($valor_evento * 0.6)/100;
-
+    $valor_liquidacion = ($valor_evento * 0.6)/100;
   }elseif ($valor_liquidacion  > $valor_tarifa_evento_8458 ) {
     $valor_tarifa =($valor_evento * 0.4)/100;
-
+    $valor_liquidacion =($valor_evento * 0.4)/100;
   }
-
 
 
    $data = $webform_submission->getData();
--- webform_handler_rcd_generador/src/Plugin/WebformHandler/GeneradorWebformHandler.php
+++ webform_handler_rcd_generador/src/Plugin/WebformHandler/GeneradorWebformHandler.php
@@ -199,13 +199,13 @@ public function money_format_fild($money) {
     $valor_liquidacion_r = 10917550 *   $numero_dias * $cantidad ;
   }elseif ($valor_liquidacion  > $valor_tarifa_evento_2115  && $valor_liquidacion <= $valor_tarifa_evento_8458) {
     $valor_tarifa =($valor_evento * 0.5)/100;
-
-  }elseif ($valor_liquidacion  = $valor_tarifa_evento_2115 ) {
+    $valor_liquidacion =($valor_evento * 0.5)/100;
+  }elseif ($valor_liquidacion  == $valor_tarifa_evento_2115 ) {
     $valor_tarifa =($valor_evento * 0.6)/100;
-
+    $valor_liquidacion = ($valor_evento * 0.6)/100;
   }elseif ($valor_liquidacion  > $valor_tarifa_evento_8458 ) {
     $valor_tarifa =($valor_evento * 0.4)/100;
-
+    $valor_liquidacion =($valor_evento * 0.4)/100;
   }
 
 
