3acff70d6dea1c02b6e62fb66ae0937418fe0379
JuanCanchila <Juan.canchila@gmail.com>
2025-02-01 10:08:09 -0500

    Igual y Superior a 200 SMMV e inferior a 300 SMMV

--- webform_handler_validate_dates/src/Plugin/WebformHandler/ValidateDatesWebformHandler.php
+++ webform_handler_validate_dates/src/Plugin/WebformHandler/ValidateDatesWebformHandler.php
@@ -187,8 +187,8 @@ public function money_format_fild($money) {
   $valor_liquidacion = 1386587 * $numero_dias;
   $valor_liquidacion_r =  951800  *  $numero_dias ;
 }elseif ($valor_liquidacion  >= $valor_tarifa_evento_200  && $valor_liquidacion < $valor_tarifa_evento_300) {
-    $valor_tarifa = 1972015;
- $valor_liquidacion = 1972015 *  $numero_dias;
+    $valor_tarifa = 2080284;
+ $valor_liquidacion = 2080284 *  $numero_dias;
  $valor_liquidacion_r =  1428000  *  $numero_dias ;
 }elseif ($valor_liquidacion  >= $valor_tarifa_evento_300  && $valor_liquidacion < $valor_tarifa_evento_400) {
     $valor_tarifa = 2629573;
